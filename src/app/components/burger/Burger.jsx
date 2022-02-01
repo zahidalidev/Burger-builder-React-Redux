cf97f457e7037d383b79debbbe9bd34b28e95677
zahidalidev <m.zahidalidev@gmail.com>
2022-02-01 13:12:38 +0500

    burger home

--- src/app/components/burger/Burger.jsx
+++ src/app/components/burger/Burger.jsx
@@ -4,19 +4,22 @@ import PropsTypes from 'prop-types'
 import './Burger.css'
 
 function Burger({ ingredients }) {
+  const ingreExist =
+    ingredients[0].list.length > 0 ||
+    ingredients[1].list.length > 0 ||
+    ingredients[2].list.length > 0 ||
+    ingredients[3].list.length > 0
+
   return (
     <div className='Burger'>
       <div className='BreadTop'>
         <div className='Seeds1'></div>
         <div className='Seeds2'></div>
       </div>
-      {(ingredients[0].list ||
-        ingredients[1].list ||
-        ingredients[2].list ||
-        ingredients[3].list) && <p>No Ingredients Added</p>}
+      {!ingreExist ? <p>No Ingredients Added</p> : null}
       {ingredients.map(item =>
-        item.list.map(() => (
-          <div key={item.id.toString()} className={`BurgerIngredient ${item.name}`}></div>
+        item.list.map((item2, i) => (
+          <div key={i.toString()} className={`BurgerIngredient ${item.name}`}></div>
         ))
       )}
       <div className='BreadBottom'></div>
